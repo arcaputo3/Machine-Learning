8f536a1da6a6f16e182520383e828be976a4800c
Albert Richard Caputo III <rcaputo3@outlook.com>
2018-09-27 11:13:47 -0400

    Modified KMeans.py

--- KMeans.py
+++ KMeans.py
@@ -1,11 +1,22 @@
 from sklearn.cluster import KMeans
+from sklearn.metrics import mean_squared_error
+import itertools
+from sklearn.metrics import silhouette_samples, silhouette_score
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.sparse import csr_matrix
-import helper
 
 data = pd.read_csv('bmi_and_life_expectancy.csv', index_col='Country')
+
+def clustering_errors(k, data):
+    kmeans = KMeans(n_clusters=k).fit(data)
+    predictions = kmeans.predict(data)
+    #cluster_centers = kmeans.cluster_centers_
+    # errors = [mean_squared_error(row, cluster_centers[cluster]) for row, cluster in zip(data.values, predictions)]
+    # return sum(errors)
+    silhouette_avg = silhouette_score(data, predictions)
+    return silhouette_avg
 
 def plot_clusters(data, n):
     X = data.values
@@ -17,12 +28,37 @@ def plot_clusters(data, n):
     predictions = kmeans_1.predict(X)
 
     # Plot
-    #draw_clusters(data.iloc[:,:2], predictions)
     clustered = pd.concat([data.reset_index(), pd.DataFrame({'group':predictions})], axis=1)
     plt.scatter(X[:,0], X[:,1], c=clustered['group'], s=20, cmap='viridis')
     plt.xlabel(data.columns[0])
     plt.ylabel(data.columns[1])
     plt.show()
 
+
+'''
+# Choose the range of k values to test.
+# We added a stride of 5 to improve performance. We don't need to calculate the error for every k value
+possible_k_values = range(2, 30, 2)
+
+# Calculate error values for all k values we're interested in
+X = data.values
+errors_per_k = [clustering_errors(k, X) for k in possible_k_values]
+
+# Plot the each value of K vs. the silhouette score at that value
+fig, ax = plt.subplots(figsize=(16, 6))
+ax.set_xlabel('K - number of clusters')
+ax.set_ylabel('Silhouette Score (higher is better)')
+ax.plot(possible_k_values, errors_per_k)
+
+# Ticks and grid
+xticks = np.arange(min(possible_k_values), max(possible_k_values)+1, 5.0)
+ax.set_xticks(xticks, minor=False)
+ax.set_xticks(xticks, minor=True)
+ax.xaxis.grid(True, which='both')
+yticks = np.arange(round(min(errors_per_k), 2), max(errors_per_k), .05)
+ax.set_yticks(yticks, minor=False)
+ax.set_yticks(yticks, minor=True)
+ax.yaxis.grid(True, which='both')
+'''
 # Play around with different params to understand data
-plot_clusters(data,10)
+plot_clusters(data,6)
